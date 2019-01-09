b27990e6b11dd5a30f86f8ac5553fbd547fc905a
Dare Lawal <lawaldare@gmail.com>
2019-01-09 17:23:37 +0100

    tested Student constructor

--- app.test.js
+++ app.test.js
@@ -1,6 +1,7 @@
 const Library = require('./app.js');
 const Person = require('./app.js');
 const Teacher = require('./app.js');
+const Student = require('./app.js');
 
 
 //Testing the Library Contsructor
@@ -22,4 +23,12 @@ test('works', () => {
   const person = new Teacher('David Kingsley', 48);
   expect(person.name).toBe('David Kingsley');
   expect(person.age).toBe(48);
+});
+
+//Testing the Student Constructor
+test('works', () => {
+  const person = new Student('Kazeem', 18, 'junior');
+  expect(person.name).toBe('Kazeem');
+  expect(person.age).toBe(18);
+  expect(person.status).toBe('junior');
 });
