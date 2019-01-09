27e609051cb61fdb99546072f23ebbddd4b06284
Dare Lawal <lawaldare@gmail.com>
2019-01-09 17:17:22 +0100

    tested Person constructor

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let library = [];
 
 //library constructor
 function Library(title) {
-  this.title = title
+  this.title = title;
   library.push(this.title);
-  return library;
+  //return library;
 }
 
 
--- app.test.js
+++ app.test.js
@@ -1,7 +1,24 @@
 const Library = require('./app.js');
+const Person = require('./app.js');
 
 
+//Testing the Library Contsructor
 test('works', () => {
   const book = new Library('item-1');
   expect(book.title).toBe('item-1');
+});
+
+
+//Testing the Person Constructor
+test('works', () => {
+  const person = new Person('Jack Mo', 28);
+  expect(person.name).toBe('Jack Mo');
+  expect(person.age).toBe(28);
+});
+
+//Testing the Teacher Constructor
+test('works', () => {
+  const person = new Person('Jack Mo', 28);
+  expect(person.name).toBe('Jack Mo');
+  expect(person.age).toBe(28);
 });
