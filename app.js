5eda081abac155035179be961b913c0dd8551a51
Dare Lawal <lawaldare@gmail.com>
2019-01-09 13:44:01 +0100

    person borrow method created

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ function Person(name, age){
   this.age = age;
   this.hasOrderBook = false;
 }
+
+Person.prototype.borrowBook = function(book){
+    if(checkAvailability(book)) {
+        this.hasOrderBook = true;
+        library = arrayRemove(library, book);
+        return library;
+    }else {
+      console.log('book-taken')
+    }
+}
