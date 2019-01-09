8b16663ff7ad9d5bd9b03b2f81040e75da35c492
Dare Lawal <lawaldare@gmail.com>
2019-01-09 13:43:10 +0100

    person constructor instatiated

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function Library(title) {
   this.title = title
   library.push(this.title);
 }
+
+//Person contructor
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.hasOrderBook = false;
+}
