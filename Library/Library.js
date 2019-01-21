f70059f9f860376ed9960a80652cee2a8affe9df
Dare Lawal <lawaldare@gmail.com>
2019-01-21 13:28:52 +0100

    added comment

--- Library/Library.js
+++ Library/Library.js
@@ -5,43 +5,39 @@ const juniorStudent = require('../Person/Student/JuniorStudent/JuniorStudent');
 const returnArray = require('../Store/returnArray');
 
 //Libarian Constructor
-function Libarian(name){
-  this.name = name;
+function Libarian(name) {
+	this.name = name;
 }
-
 
 //grantRequest method
-Libarian.prototype.grantRequest = function(){
-  for(let book of library){
-    if((requestArray[0].title === book.title) && (book.quantity >= 1) && (requestArray[0].author === book.author)){
-      book.userID = requestArray[0].idN;
-      borrowedBook.push(book);
-      requestArray.splice(0, 1);
-      book.quantity = book.quantity - 1;
-      return borrowedBook;
-    } else if ((requestArray[0].title === book.title) && (book.quantity < 1)){
-      requestArray.splice(0, 1);
-      return `${book.title} not available`;
-    }
-  }
-}
-
+Libarian.prototype.grantRequest = function() {
+	for (let book of library) {
+		if (requestArray[0].title === book.title && book.quantity >= 1 && requestArray[0].author === book.author) {
+			//checking if the book requested is in the Library
+			book.userID = requestArray[0].idN;
+			borrowedBook.push(book); //Pushing the book reference in the borrowedbook box
+			requestArray.splice(0, 1); // removing the book reference for the request array
+			book.quantity = book.quantity - 1;
+			return borrowedBook;
+		} else if (requestArray[0].title === book.title && book.quantity < 1) {
+			requestArray.splice(0, 1);
+			return `${book.title} not available`; // incase the book ain't the library
+		}
+	}
+};
 
 //acceptBook method
-Libarian.prototype.acceptBook = function(){
-  for(let book of returnArray){
-    for(let bookie of library){
-      if((book.title === bookie.title) && (book.author === book.author)){
-        book.quantity = book.quantity + 1;
-        returnArray.splice(0, 1);
-        return book.quantity;
-      } 
-    }  
-  }
-} 
+Libarian.prototype.acceptBook = function() {
+	for (let book of returnArray) {
+		for (let bookie of library) {
+			if (book.title === bookie.title && book.author === book.author) {
+				//checking if the book reference from the libarary
+				book.quantity = book.quantity + 1;
+				returnArray.splice(0, 1); //removing the book reference for the request array
+				return book.quantity;
+			}
+		}
+	}
+};
 
-
-
-
-
-module.exports = Libarian
+module.exports = Libarian;
--- Person/Student/JuniorStudent/JuniorStudent.js
+++ Person/Student/JuniorStudent/JuniorStudent.js
@@ -3,17 +3,11 @@ const requestArray = require('../../../Store/requestArray');
 const returnArray = require('../../../Store/returnArray');
 const borrowedBook = require('../../../Store/borrowedBook');
 
-
 //Student Constructor
-function Student(name, idN, priority){
-  Person.call(this, name, idN, priority); 
+function Student(name, idN, priority) {
+	Person.call(this, name, idN, priority);
 }
-
-
-
 
 Student.prototype = Object.create(Person.prototype);
 
-
-
-module.exports = Student
+module.exports = Student;
